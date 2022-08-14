1a9c9d7b5cb3da551a154ed274b49e93fe5b9132
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-08-14 19:17:09 +0530

    Validate form data through CSS pseudoclasses, HTML attributes and JS

---
+++ forms/form-validation/script.js
@@ -0,0 +1,25 @@
+const myform = document.getElementById("myform");
+myform.noValidate = true;
+
+function validateForm(e) {
+    const form = e.target;
+    if (form.checkValidity()) {
+        // form is valid - make further checks
+    }
+    else {
+        // form is invalid - cancel submit
+        e.preventDefault();
+        // apply invalid class
+        Array.from(form.elements).forEach(i => {
+            if (i.checkValidity()) {
+                // field is valid - remove class
+                i.parentElement.classList.remove('invalid');
+            }
+            else {
+                // field is invalid - add class
+                i.parentElement.classList.add('invalid');
+            }
+        });
+    }
+};
+myform.addEventListener("submit", validateForm);
